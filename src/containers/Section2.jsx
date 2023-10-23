f0d7d87c6f94f02de48f399b58aca3cd8c0cb468
JayDeep-Giri <jaydeepgiri30@gmail.com>
2023-10-23 11:56:11 +0530

    Initialized and Updated 1inch-Clone

---
+++ src/containers/Section2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const Section2 = () => {
+  return (
+    <Box sx={{height: 500}}>Section2</Box>
+  )
+}
+
+export default Section2
